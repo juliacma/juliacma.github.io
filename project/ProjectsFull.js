b83d522b3aef31984f9bb42aab452e42893b762f
Julia <julia.ma@yale.edu>
2019-02-20 02:19:28 -0500

    second commit

---
+++ project/ProjectsFull.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import Aux from '../hoc/Aux';
+import Data from './ProjectData';
+import styles from './main.css';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+
+class projectsfull extends Component {
+
+    constructor(props) {
+        super(props)
+
+
+        let id = props.match.params.id
+        let post  = NaN
+        for(let i = 0; i < Data.length; i++) {
+            if (Data[i].id === id) {
+                post = Data[i];
+            }
+        }
+
+        this.state = {
+            title : post.title,
+            subtitle : post.subtitle,
+            image : post.image,
+            description : post.long,
+        }
+    }
+    
+    render() {
+        return (
+            <Aux>
+                <div>
+                    <p className={styles.back}><Link onClick={this.props.handleBack2Grid} exact to="/about/highlights">&lsaquo; Back to Highlights</Link></p>
+                    <h3 dangerouslySetInnerHTML={{__html: this.state.title}} className={styles.title}></h3>
+                    <img src={this.state.image} alt="Spotlight" className={classNames("imageKing", styles.imageFull)} ></img>
+                    <p dangerouslySetInnerHTML={{__html: this.state.description}} className={styles.description}></p>
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default highlightsfull;
